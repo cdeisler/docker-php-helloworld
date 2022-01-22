6ea619208efe6a7993c91ac1866a3c94b4a85061
cdeisler <c33jd13s3l@gmail.com>
2022-01-22 16:45:36 -0500

    test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM php:7.0-apache
 COPY src/ /var/www/html
 EXPOSE 80
-
+RUN echo "ServerName localhost:80" >> /etc/apache2/apache2.conf
--- src/index.php
+++ src/index.php
@@ -1,7 +1,21 @@
-<?php
+<?php  
+$dir = "D:\RetroPie\RetroPieSync\192.168.1.148\roms\arcade";//"/home/pi/RetroPie/roms/arcade";    
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+function scan($dir){ 
+   $result = array(); 
+   foreach(scandir($dir) as $key => $value){ 
+      if(!empty($value) and !in_array($value, array(".", ".."))){ 
+         if(is_dir($dir.DIRECTORY_SEPARATOR.$value)){ 
+            $result[$value] = scan($dir.DIRECTORY_SEPARATOR.$value); 
+         } 
+         else{ 
+            $result[] = $value; 
+         } 
+      } 
+   }  
+   return $result; 
+} 
+
+echo json_encode((array)scan($dir), JSON_UNESCAPED_UNICODE);
 
 ?>
